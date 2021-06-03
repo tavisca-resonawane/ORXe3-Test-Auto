e1adc108772129d945b70275baf252decef9f850
Reema Sonawane <resonawane@affinionds.com>
2021-06-03 16:41:14 +0530

    Revert "added ts and gitingore files"
    This reverts commit 6f02aa0f9d44261581e63b481a68122197aeebf7.

--- TypeScript/assi_3.2/test/pageobjects/hotelDetails.page.ts
+++
@@ -1,8 +0,0 @@
-class HotelDetailsPage
-{
-        get headerHotelName(){return $('.hotel-redesign-detail-header-main h2 span')}
-        get btnBookRoom(){return $$('button[data-qaid=\'Button_BookRoom\']')}
-        get hoteldetailsstarrating(){return $('.hotel-details-star-rating-container')}
-
-}
-export default new HotelDetailsPage();
